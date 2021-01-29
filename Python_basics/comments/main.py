2e8e40c4eccc10ad8f2a1f51b43d05de0f7f1017
Darya van Tienhoven <daryaheleen@gmail.com>
2021-01-29 13:53:17 +0100

    opdrachten gemaakt

---
+++ Python_basics/comments/main.py
@@ -0,0 +1,22 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+#Calculate age based on three variables:
+current_year = 2021
+year_of_birth = 1986
+had_birthday_this_year = False #my birthday is in september
+
+if(had_birthday_this_year): #I added the if-else-statement after I realized the code had just made me a year older :(
+    age = current_year - year_of_birth 
+else:
+    age = current_year - year_of_birth -1 
+
+print(age)
+
+""" for this exercise I had to look up how
+to write an if else statement in Python """
+"""it seems simpler and more intuitive than the 
+if else statements in JavaScript """
